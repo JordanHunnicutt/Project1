b3592c5151600ce40382125152b8a74e97d5bac4
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-10-30 11:02:50 -0400

    removed empty setup and import

--- src/test/java/com/web/service/UserServiceTest.java
+++ src/test/java/com/web/service/UserServiceTest.java
@@ -1,21 +1,12 @@
 package com.web.service;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import com.web.models.User;
-import com.web.repository.UserDao;
 
 public class UserServiceTest {
 
 	private static final UserService us = new UserService();
-	
-	@Before
-	public void setup(){
-	}
 	
 	@Test
 	public void loginServiceTest() {		
